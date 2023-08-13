f1e7e21db1a156dba23a76f7d9652168593bb6af
gngsn <rudtjs4540@gmail.com>
2023-08-14 00:54:08 +0900

    update: modify content

--- src/main/kotlin/com/gngsn/ditto/shared/model/PagingCommand.kt
+++ src/main/kotlin/com/gngsn/ditto/shared/model/PagingCommand.kt
@@ -1,6 +1,6 @@
 package com.gngsn.ditto.shared.model
 
 class PagingCommand(
-    val page: Long,
-    val size: Long
+    val page: Long = 0,
+    val size: Long = 10
 )
--- src/test/kotlin/com/gngsn/ditto/adapter/external/ArticlePersistenceAdapterTest.kt
+++ src/test/kotlin/com/gngsn/ditto/adapter/external/ArticlePersistenceAdapterTest.kt
@@ -3,8 +3,11 @@ package com.gngsn.ditto.adapter.external
 import com.gngsn.ditto.adapter.external.persistence.repository.ArticleMasterRepository
 import com.gngsn.ditto.adapter.external.persistence.repository.ArticleSlaveRepository
 import com.gngsn.ditto.domain.Article
+import com.gngsn.ditto.shared.model.PagingCommand
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import jakarta.persistence.EntityManager
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -12,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(MockKExtension::class)
 class JpaQuerydslTest {
+    @MockK lateinit var entityManager: EntityManager
     @MockK lateinit var articleMasterRepository: ArticleMasterRepository
     @MockK lateinit var articleSlaveRepository: ArticleSlaveRepository
     private lateinit var articlePersistenceAdapter: ArticlePersistenceAdapter
@@ -24,6 +28,10 @@ class JpaQuerydslTest {
     @Test
     fun select_article() {
         val article = Article( "새로운 게시글", "내용입니다", "sunny")
-        articlePersistenceAdapter.save(article)
+        every {
+            articlePersistenceAdapter.save(article)
+        } answers {
+            articlePersistenceAdapter.findAll(PagingCommand())
+        }
     }
 }
