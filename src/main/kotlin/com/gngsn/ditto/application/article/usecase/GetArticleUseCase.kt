ec83d4290cf5f0c0205a0073005fc0d2b765bd77
gngsn <rudtjs4540@gmail.com>
2023-08-20 00:03:54 +0900

    Refac: remove service layer and modify usecase from interface to class

--- src/main/kotlin/com/gngsn/ditto/application/article/usecase/GetArticleUseCase.kt
+++ src/main/kotlin/com/gngsn/ditto/application/article/usecase/GetArticleUseCase.kt
@@ -1,8 +1,16 @@
 package com.gngsn.ditto.application.article.usecase
 
+import com.gngsn.ditto.application.article.mapper.toVO
 import com.gngsn.ditto.domain.Article
+import com.gngsn.ditto.port.output.GetArticleListPort
 import com.gngsn.ditto.shared.model.PagingCommand
+import com.gngsn.ditto.shared.support.UseCase
 
-interface GetArticleUseCase {
-    fun getList(pagingCommand: PagingCommand): List<Article>
+@UseCase
+class GetArticleUseCase(
+    val getArticleListPort: GetArticleListPort
+) {
+    fun getList(pagingCommand: PagingCommand): List<Article> {
+        return getArticleListPort.findAll(pagingCommand).map { it.toVO() }
+    }
 }
--- src/main/kotlin/com/gngsn/ditto/application/article/usecase/SaveArticleUseCase.kt
+++ src/main/kotlin/com/gngsn/ditto/application/article/usecase/SaveArticleUseCase.kt
@@ -1,7 +1,17 @@
 package com.gngsn.ditto.application.article.usecase;
 
 import com.gngsn.ditto.domain.Article
+import com.gngsn.ditto.port.output.SaveArticlePort
+import com.gngsn.ditto.shared.support.UseCase
+import jakarta.transaction.Transactional
 
-interface SaveArticleUseCase {
-    fun save(article: Article)
+@UseCase
+class SaveArticleUseCase(
+    val saveArticlePort: SaveArticlePort
+) {
+
+    @Transactional
+    fun save(article: Article) {
+        saveArticlePort.save(article)
+    }
 }
