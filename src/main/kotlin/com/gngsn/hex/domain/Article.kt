99549eb1b62f367df2714ebace55cf9744b40d1e
gngsn <enfj.dev@gmail.com>
2023-08-31 00:18:47 +0900

    Modify: change domain vo

--- src/main/kotlin/com/gngsn/hex/domain/Article.kt
+++ src/main/kotlin/com/gngsn/hex/domain/Article.kt
@@ -6,7 +6,8 @@ import jakarta.validation.constraints.NotBlank
  * Article domain
  */
 data class Article(
-    @field:NotBlank(message = "'title' must not be blank!") val title: String,
-    @field:NotBlank(message = "'content' must not be blank!") val content: String,
-    @field:NotBlank(message = "'author' must not be blank!") val author: String,
-)
+    val title: String,
+    val content: String,
+    val author: String,
+) {
+}
